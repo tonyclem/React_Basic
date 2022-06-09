046827515cca4636c3758e392bc7859b209a1925
tonyclem <tonyclem001@gmail.com>
2022-06-09 16:21:36 +0200

    adding useState Array example

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import { UseStateBasic } from "./tutorial/useState/setup/useState-basic";
+import Setup from "./tutorial/useState/setup/useState-array";
 
 const App = () => {
   return (
     <div className="container">
-      <UseStateBasic />
+      <Setup />
     </div>
   );
 };
